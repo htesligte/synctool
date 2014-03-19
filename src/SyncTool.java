badb29ce106e3de1192e5e2645a654e351f1063f
Henk te Sligte <henk@hjts.nl>
2014-03-19 22:56:23 +0100

    div wijzigingen thuis

--- src/SyncTool.java
+++ src/SyncTool.java
@@ -1,15 +1,20 @@
+import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.PrintCommandListener;
 
 public class SyncTool
 {
@@ -70,6 +75,7 @@ public class SyncTool
     public static void downloadFiles()
     {
         try {
+            String userinput;
             int siteId = selectSite();
             Element site = (Element) sites.item(siteId);
 
@@ -77,16 +83,67 @@ public class SyncTool
             String newsite = sitename.substring( 0, sitename.indexOf( "." ) ) + ".hts";
 
             String msg = "Nieuwe site [" + newsite + "]: ";
-            String userinput = scanner.nextLine();
+            userinput = scanner.nextLine();
             if( userinput.length() > 0 )
                 newsite = userinput;
 
-            String directory = site.getElementsByTagName("files").item(0).getTextContent();
+            String sLocalDir = "/home/henk/web/" + newsite;
+            File LocalDir = getDownloadDir( new File( sLocalDir ) );
+
+
+            String directory = site.getElementsByTagName("directory").item(0).getTextContent();
+
+            String username = site.getElementsByTagName( "username" ).item(0).getTextContent();
+            String password = site.getElementsByTagName( "password" ).item(0).getTextContent();
+
+            FTPClient ftp = new FTPClient();
+            ftp.connect( "94.126.71.123" );
+            ftp.login( username, password );
+            ftp.addProtocolCommandListener( new PrintCommandListener( new PrintWriter( System.out ) ) );
+            ftp.enterLocalPassiveMode();
+            ftp.changeWorkingDirectory(directory);
+
+            File LocalDir = new File( "/home/henk/web/" + newsite );
 
 
         } catch( Exception e ) {
             e.printStackTrace();
         }
+    }
+
+    public static File getDownloadDir( File requestedDir )
+    {
+        try {
+            if( !requestedDir.exists() )
+                return requestedDir;
+
+            String userinput;
+            System.out.println( "Directory " + requestedDir.getName() + " bestaat al. (R)enamen of (V)erwijderen?" );
+            userinput = scanner.nextLine();
+            if( userinput.equalsIgnoreCase("r") )
+            {
+                DateFormat dateFormat = new SimpleDateFormat( "ddMMyyyy-HHmmss" );
+                Date date = new Date();
+
+                String newdirname = requestedDir.getName() + "-" + dateFormat.format(date);
+                System.out.println( "Nieuwe dirname: [" + newdirname + "]" );
+                userinput = scanner.nextLine();
+                if( userinput.length() > 0 )
+                    newdirname = userinput;
+
+                requestedDir.renameTo( new File( "/home/henk/web/" + newdirname ))
+
+                return getDownloadDir( new File( "/home/henk/web/" + newdirname ) );
+            }
+            else if( userinput.equalsIgnoreCase( "v" ) )
+            {
+                FileUtils.deleteDirectory(requestedDir);
+                return requestedDir;
+            }
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void downloadDatabase()
@@ -99,7 +156,7 @@ public class SyncTool
         showSites();
     }
 
-    public static void ftpMirror( File dirname )
+    public static void ftpMirror( File LocalDir, FTPClient ftp )
     {
 
     }
